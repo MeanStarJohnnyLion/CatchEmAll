bae185fcd593c1d69f4d7c0bd03b750fe1b92f9e
Johnny Morgan <johnnylion3@aol.com>
2022-10-29 09:34:31 -0400

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Johnny Lion on 10/29/22.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
